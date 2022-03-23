b22673c46e38df10dafc5444c538e81ce25844ff
kobaruto sato <satocobalt@gmail.com>
2022-03-24 06:42:19 +0900

    サブテスト

--- mock/unit_test/hey_test.go
+++ mock/unit_test/hey_test.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-  "testing"
+	"testing"
+
 	"github.com/CobaltSato/mock/unit_test/hey"
 )
 
-// $ go test github.com/CobaltSato/mock/unit_test -v # gomod記載のパッケージ名を指定する
-// $ go test . -v # もしくはカレントディレクトリ指定でもOK
+// # gomod記載のパッケージ名を指定する
+// go test github.com/CobaltSato/mock/unit_test -v
+// # もしくはカレントディレクトリ指定でもOK
+// go test . -v
 
+// サブテスト
+// go test github.com/CobaltSato/mock/unit_test -run TestHeyDo/A  -v
+// go test github.com/CobaltSato/mock/unit_test -run TestHeyDo/B  -v
 
 func TestHeyDo(t *testing.T) {
-  want := "こんにちは"
-  got := hey.Do()
-  if got != want {
-    t.Errorf("want %q, got %q", want, got)
-  }
+	t.Run("A", func(t *testing.T) {
+  	want := "こんにちは"
+  	got := hey.Do()
+  	if got != want {
+  	  t.Errorf("want %q, got %q", want, got)
+  	}
+	})
+	t.Run("B", func(t *testing.T) {
+  	want := "hello"
+  	got := hey.Do()
+  	if got != want {
+  	  t.Errorf("want %q, got %q", want, got)
+  	}
+	})
 }
 
 
