111f97a86bbdf09d6e745ae310bf1cb9978eb9da
kobaruto sato <satocobalt@gmail.com>
2022-03-24 07:27:54 +0900

    is_evenテスト

---
+++ mock/unit_test/is_even/is_even.go
@@ -0,0 +1,5 @@
+package is_even
+
+func IsEven(number int) bool {
+  return (number % 2 == 0);
+}
--- mock/unit_test/is_even/is_even_test.go
+++ mock/unit_test/is_even/is_even_test.go
@@ -3,14 +3,15 @@
 
 package is_even_test
 
-import ("testing")
-
-func isEven(number int) bool {
-  return (number % 2 == 0);
-}
+import (
+	"testing"
+  "github.com/CobaltSato/mock/unit_test/is_even"
+)
 
 func TestIsEven(t *testing.T) {
 	t.Parallel()
+
+	// テストケース
 	cases := map[string]struct {in int; want bool}{
 		"+odd":  {5, false},
 		"+even": {6, true},
@@ -18,11 +19,12 @@ func TestIsEven(t *testing.T) {
 		"-even": {-6, true},
 		"zero":  {0, true},
 	}
+
 	for name, tt := range cases {
-		tt := tt // ローカル変数
-		t.Run(name, func(t *testing.T) {
+		tt := tt // ローカル変数にコピー
+		t.Run(name, func(t *testing.T) { // サブテストとして実行
 			t.Parallel() // 並列実行
-			if got := isEven(tt.in); tt.want != got {
+			if got := is_even.IsEven(tt.in); tt.want != got {
 				t.Errorf( "want IsEven(%d) = %v, got %v", tt.in, tt.want, got)
       }
     })
